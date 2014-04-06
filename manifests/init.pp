60152dab13453df848e4d42e130b056379b9108e
Garrett Honeycutt <code@garretthoneycutt.com>
2014-04-06 16:26:47 +0200

    Simplify type and value checks
    Use validate_re() instead of checking type and use case statements to
    validate code. This greatly simplies things.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,33 +8,8 @@ class selinux (
   $config_file = '/etc/selinux/config',
 ) {
 
-  $is_mode_a_string = is_string($mode)
-  if $is_mode_a_string == false {
-    fail('selinux::mode is not a string')
-  }
-
-  $is_type_a_string = is_string($type)
-  if $is_type_a_string == false {
-    fail('selinux::type is not a string')
-  }
-
-  case $mode {
-    'enforcing','permissive','disabled': {
-      # noop
-    }
-    default: {
-      fail("mode is ${mode} and must be either 'enforcing', 'permissive' or 'disabled'.")
-    }
-  }
-
-  case $type {
-    'targeted','strict': {
-      # noop
-    }
-    default: {
-      fail("type is ${type} and must be either 'targeted' or 'strict'.")
-    }
-  }
+  validate_re($mode, '^enforcing|permissive|disabled$', "mode is ${mode} and must be either 'enforcing', 'permissive' or 'disabled'.")
+  validate_re($type, '^targeted|strict$', "type is ${type} and must be either 'targeted' or 'strict'.")
 
   file { 'selinux_config':
     ensure  => 'file',
