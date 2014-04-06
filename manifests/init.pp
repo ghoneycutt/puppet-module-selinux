f4118d66af5ea6574883d50bd4687f2b4bc89d71
Garrett Honeycutt <code@garretthoneycutt.com>
2014-04-06 16:22:48 +0200

    Update README for consistency with other modules
    Remove duplicate info from manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,7 @@
-# ## selinux module #
+# == Class: selinux
 #
 # This module manages the SELinux configuration file.
 #
-# ### Parameters ###
-#
-# See man page selinux(8) for more information regarding the configuration settings.
-#
-# mode
-# ----
-# Operation mode of SELinux, valid values are 'enforcing', 'permissive' and 'disabled'.
-#
-# - *Default*: 'disabled'
-#
-# type
-# ----
-# The type of policies in use, valid values are 'targeted' and 'strict'.
-#
-# - *Default*: 'targeted'
-#
-# config_file
-# -----------
-# The path to the selinux configuration path to manage.
-#
-# - *Default*: '/etc/selinux/config'
-#
-
 class selinux (
   $mode        = 'disabled',
   $type        = 'targeted',
