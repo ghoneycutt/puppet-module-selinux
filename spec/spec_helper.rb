bc2458f8e25e6758e9238b7606103a022cdbd664
Garrett Honeycutt <code@garretthoneycutt.com>
2018-12-12 13:56:26 -0500

    Remove unneeded configuration from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ end
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
-  config.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
   config.before :each do
     # Ensure that we don't accidentally cache facts and environment between
     # test cases.  This requires each example group to explicitly load the
