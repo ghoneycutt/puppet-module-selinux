24162526375eb3c7192a11ef9fcf071a58a25408
Garrett Honeycutt <code@garretthoneycutt.com>
2018-12-12 21:10:16 -0500

    Add Vagrant testing

---
+++ vagrant/provision_basic_el.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# using this instead of "rpm -Uvh" to resolve dependencies
+function rpm_install() {
+    package=$(echo $1 | awk -F "/" '{print $NF}')
+    wget --quiet $1
+    yum install -y ./$package
+    rm -f $package
+}
+
+release=$(awk -F \: '{print $5}' /etc/system-release-cpe)
+
+rpm --import http://download-ib01.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-${release}
+rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppet
+rpm --import http://vault.centos.org/RPM-GPG-KEY-CentOS-${release}
+
+yum install -y wget
+
+# install and configure puppet
+rpm -qa | grep -q puppet
+if [ $? -ne 0 ]
+then
+
+    rpm_install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-${release}.noarch.rpm
+    yum -y install puppet-agent
+    ln -s /opt/puppetlabs/puppet/bin/puppet /usr/bin/puppet
+
+    # suppress default warnings for deprecation
+    cat > /etc/puppetlabs/puppet/hiera.yaml <<EOF
+---
+version: 5
+hierarchy:
+  - name: Common
+    path: common.yaml
+defaults:
+  data_hash: yaml_data
+  datadir: hieradata
+EOF
+
+fi
+
+# use local selinux module
+puppet resource file /etc/puppetlabs/code/environments/production/modules/selinux ensure=link target=/vagrant
+
+# setup module dependencies
+puppet module install puppetlabs/stdlib --version 4.24.0
